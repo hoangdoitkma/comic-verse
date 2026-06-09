e805c85822d013a8e28fe55e5418048a5fe247f3
hoangdoitkma <hoangdoitkma@gmail.com>
2026-06-10 01:00:28 +0700

    fix: point clients to nginx ip

--- web-admin/src/utils/axiosClient.js
+++ web-admin/src/utils/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiBaseURL = import.meta.env.VITE_API_BASE_URL || '/api';
+const apiBaseURL = import.meta.env.VITE_API_BASE_URL || 'http://3.1.207.156/api';
 
 const axiosClient = axios.create({
   baseURL: apiBaseURL,
